036906e19663d0b31d24f7c19882465a6e4b8fca
Noah <98564568+THatMustacheCat@users.noreply.github.com>
2025-09-17 13:54:53 -0400

    YAYYYYY... I mean I did it

--- student.py
+++ student.py
@@ -1,5 +1,17 @@
 # TODO create class Student
+class Student:
+    def __init__(self, name, grade):
+        self.n = name
+        self.g = grade
 
+    def get_name(self):
+        return self.n
+
+    def get_grade(self):
+        return self.g
+
+    def set_grade(self, new):
+        self.g = new
 if __name__ == "__main__":
     # create Student below this
     pass
